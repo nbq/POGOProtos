4e90cc4e3e058278b731078c3fa2dc99c532d647
goedzo <git@nims.nl>
2017-09-09 15:51:40 +0200

    Generated all files

---
+++ _build docker_pgo-api.bat
@@ -0,0 +1,5 @@
+cd "G:\Setups\Pokemon Go\Bot\Pikabot\_git\POGOProtos\goedzo"
+docker build --build-arg BUILD_BRANCH=dev -t pokemongo-api . 
+pause
+docker images
+pause
---
+++ _connect_pgo-api.bat
@@ -0,0 +1,2 @@
+docker run --name=pokemongo-api-bash --rm -i -t -e TZ=Europe/Amsterdam -v "G:/Setups/Pokemon Go/Bot/Pikabot/_git/POGOProtos/output/":/tmp/ --entrypoint /bin/bash pokemongo-api
+pause
