abde5b1fb711357edbd96ff807ffb6f96e29feb3
Jason Hawks <mail@netbit.me>
2017-10-27 00:52:03 -0400

    fixed for docker toolbox

--- custom_build/pogo-docker-build.bat
+++ custom_build/pogo-docker-build.bat
@@ -1,5 +1,7 @@
+docker-machine env default
+@FOR /f "tokens=*" %i IN ('docker-machine env default') DO @%i
 cd ..
-docker build --build-arg BUILD_BRANCH=master BUILD_REPO="nbq/POGOProtos" -t pokemongo-api . 
+docker build --build-arg BUILD_BRANCH=master --build-arg BUILD_REPO="nbq/POGOProtos" -t pokemongo-api . 
 pause
 docker images
 pause
