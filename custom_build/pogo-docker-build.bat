321da518957cda26732c7b52456ac7a58d6002c4
Jason Hawks <mail@netbit.me>
2017-10-27 01:42:49 -0400

    tweak files

--- custom_build/pogo-docker-build.bat
+++ custom_build/pogo-docker-build.bat
@@ -1,7 +1,7 @@
 docker-machine env default
-@FOR /f "tokens=*" %i IN ('docker-machine env default') DO @%i
+@FOR /f "tokens=*" %%i IN ('docker-machine env default') DO @%%i
 cd ..
-docker build --build-arg BUILD_BRANCH=master --build-arg BUILD_REPO="nbq/POGOProtos" -t pokemongo-api . 
+docker build --build-arg BUILD_BRANCH=0.79.2 --build-arg BUILD_REPO=nbq/POGOProtos -t pokemongo-api . 
 pause
 docker images
 pause
