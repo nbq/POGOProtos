e2d8be062fe7b293b3153048ebd04f77c081edc6
Jason Hawks <mail@netbit.me>
2017-10-27 02:06:00 -0400

    start to simplify

---
+++ split2_proto_run.bat
@@ -0,0 +1,3 @@
+mode con: cols=200 lines=60
+Powershell.exe -File split2_proto.ps1 -Sourcefile "%cd%\base\v0.79.2.proto" -OutPath "%cd%\temp_out" -ProtoIndexFile "%cd%\proto_index.txt" -ProtoTypeFile "%cd%\proto_types.txt" -ProtoImportFile "%cd%\proto_import.txt" -SrcFolder "%cd%\..\src" -MissingFile "%cd%\proto_missing.txt" > output.txt
+pause
